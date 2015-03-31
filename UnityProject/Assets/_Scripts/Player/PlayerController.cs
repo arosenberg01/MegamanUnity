df1836f2cfd3f5a0ba64e284689d7473d2e1230e
zhen <xxgiamxx@gmail.com>
2015-03-30 19:16:35 -0700

    Added skeleton script files

--- UnityProject/Assets/_Scripts/Player/PlayerController.cs
+++ UnityProject/Assets/_Scripts/Player/PlayerController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MovementScript : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
